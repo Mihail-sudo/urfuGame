c8f44a93c9a0e031ff79b7fd2cd0a05d0974c485
Mihail-sudo <57751986+Mihail-sudo@users.noreply.github.com>
2021-04-28 11:47:49 +0500

    made a low physics

--- Game/Game/Form1.Designer.cs
+++ Game/Game/Form1.Designer.cs
@@ -29,20 +29,61 @@ namespace Game
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.platform = new System.Windows.Forms.PictureBox();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.platform)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
             this.SuspendLayout();
+            // 
+            // platform
+            // 
+            this.platform.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.platform.Location = new System.Drawing.Point(12, 410);
+            this.platform.Name = "platform";
+            this.platform.Size = new System.Drawing.Size(480, 28);
+            this.platform.TabIndex = 0;
+            this.platform.TabStop = false;
+            this.platform.Tag = "platform";
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.player.Location = new System.Drawing.Point(71, 354);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(29, 50);
+            this.player.TabIndex = 1;
+            this.player.TabStop = false;
+            this.player.Tag = "player";
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.MainGameTimerEvent);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(845, 450);
+            this.ClientSize = new System.Drawing.Size(504, 450);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.platform);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.platform)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox platform;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Timer gameTimer;
     }
 }
 
--- Game/Game/Form1.cs
+++ Game/Game/Form1.cs
@@ -12,10 +12,82 @@ namespace Game
 {
     public partial class Form1 : Form
     {
+        bool goLeft, goRight, jumping, isGameOver;
+        int jumpSpeed, force;
+        int playerSpeed = 10; 
+
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void MainGameTimerEvent(object sender, EventArgs e)
+        {
+            player.Top += jumpSpeed;
+            if (goLeft)
+                player.Left -= playerSpeed;
+            if (goRight)
+                player.Left += playerSpeed;
+            if (jumping && force < 0)
+                jumping = false;
+            if (jumping)
+            {
+                jumpSpeed = -8;
+                force -= 1;
+            }
+            else
+                jumpSpeed = 10;
+
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox)
+                {
+                    if ((string)x.Tag == "platform")
+                        if (player.Bounds.IntersectsWith(x.Bounds))
+                        {
+                            force = 8;
+                            player.Top = x.Top - player.Height;
+                        }
+                        x.BringToFront();
+                    }
+            }
+        }
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+                goLeft = true;
+            if (e.KeyCode == Keys.Right)
+                goRight = true;
+            if (e.KeyCode == Keys.Space && jumping == false)
+                jumping = true;
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>")]
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+                goLeft = false;
+            if (e.KeyCode == Keys.Right)
+                goRight = false;
+            if (jumping)
+                jumping = false;
+            if (e.KeyCode == Keys.Enter)
+                StartGame();
+        }
+
+        private void StartGame() 
+        {
+            jumping = false;
+            goLeft = false;
+            goRight = false;
+            isGameOver = false;
+
+            player.Left = 71;
+            player.Top = 354;
+
+            gameTimer.Start();
+
+        }
     }
 }
